981588914dbb9f7e33c4ff6a0c31463f669007e2
sebgrebe <sebbecker@gmx.net>
2017-12-29 16:59:36 +0100

    change db_url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
-var db_url = 'mongodb://heroku_6tx14993:lpnk0lmir126cvb4arnj3hquji@ds131997.mlab.com:31997/heroku_6tx14993' || 'mongodb://localhost:27017/nightlife'
+console.log(process.env.NODE_ENV)
+var db_url = 'mongodb://localhost:27017/nightlife' || 'mongodb://heroku_6tx14993:lpnk0lmir126cvb4arnj3hquji@ds131997.mlab.com:31997/heroku_6tx14993'
 
 module.exports = {
     'url': db_url
